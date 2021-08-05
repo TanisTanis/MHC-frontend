d027d0be4f9880686ffa5e379ce2a5c24e825a7b
Tanis Kiel <tkiel12@gmail.com>
2021-08-04 17:06:41 -0700

    Work on login page, validations, post request

--- src/app/landingpage/header/login/login.component.ts
+++ src/app/landingpage/header/login/login.component.ts
@@ -1,30 +1,47 @@
 import { Component } from '@angular/core';
 import { UserInfo } from './user';
 import { UserService } from 'src/app/user.service';
-import { validateLogIn } from '../validations';
-import { validate } from 'json-schema';
+import { validateLogIn, validatePassword } from '../validations';
+import { validateEmail } from '../validations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
   invalid: boolean = false;
   errors: String[] = [];
+  validEmail: boolean = false;
+  emailError: boolean = false;
+  validPassword: boolean = false;
 
-
-  constructor(private userService: UserService) {
-  }
+  constructor(private userService: UserService) {}
 
   model = new UserInfo('', '');
 
+  handleEmailChange() {
+    if (validateEmail(this.model.email)) {
+      this.validEmail = true;
+      this.emailError = false;
+    } else {
+      this.validEmail = false;
+      this.emailError = true;
+    }
+  }
+
+  handlePasswordChange() {
+    if (validatePassword(this.model.password)) {
+      this.validPassword = true;
+    } else {
+      this.validPassword = false;
+    }
+  }
+
   handleLogIn() {
-    console.log(this.model);
     const validLogIn = validateLogIn(this.model.email, this.model.password);
     if (validLogIn.valid) {
-      console.log('login valid')
+      console.log('login valid');
     } else {
       this.invalid = true;
       this.errors = [];
@@ -34,5 +51,4 @@ export class LoginComponent {
       }
     }
   }
-
 }
--- src/app/landingpage/header/validations.ts
+++ src/app/landingpage/header/validations.ts
@@ -1,69 +1,75 @@
-const emailRegEx = /\S+@\S+\.\S+/;
-const atCheck = /^[^\s@]+@[^\s@]+$/;
+const emailRegEx: RegExp = /\S+@\S+\.\S+/;
+const atCheck: RegExp = /^[^\s@]+@[^\s@]+$/;
 
 interface ValidationResponse {
   valid: boolean,
   errors: String[]
 }
 
+export function validateEmail(email: string) {
+  const validEmail: boolean = emailRegEx.test(email);
+  const multipleAtCheck: boolean = atCheck.test(email);
+  return multipleAtCheck && validEmail;
+}
+
+export function validatePassword(password: string) {
+  const lengthValid = password.length >= 10;
+  return lengthValid;
+}
+
+export function validatePhoneNumber(phone: string) {
+  const phoneLengthValid: boolean = phone.length >= 10;
+  return phoneLengthValid;
+}
+
 export function validateLogIn(email: string, password: string) {
-  const validEmail = emailRegEx.test(email);
-  const multipleAtCheck = atCheck.test(email);
-  const passValid = password.length > 5;
-  const allValid = validEmail && multipleAtCheck && passValid;
+  const emailValid: boolean = validateEmail(email);
+  const passValid: boolean = validatePassword(password);
+  const allValid: boolean = emailValid && passValid;
 
   const validationObj: ValidationResponse = {
     valid: allValid,
     errors: []
   }
 
-  const validations: boolean[] = [validEmail, multipleAtCheck, passValid];
+  const validations: boolean[] = [emailValid, passValid];
 
-  const errorMessages: String[] = ['Email is invalid', 'Email is invalid', 'Password is invalid'];
+  const errorMessages: String[] = ['Email is invalid', 'Password is invalid'];
 
-  for (let i = 0; i < validations.length; i++) {
-    if (!validations[i]) {
-      if (validationObj.errors.indexOf(errorMessages[i]) === -1) {
+  if (!allValid) {
+    for (let i = 0; i < validations.length; i++) {
+      if (!validations[i]) {
         validationObj.errors.push(errorMessages[i]);
       }
     }
   }
-
   return validationObj;
 }
 
 export function validateRegister(email: string, password: string, confirmPassword: string, firstName: string, lastName: string, phone: string) {
-  const validEmail: boolean = emailRegEx.test(email);
-  const multipleAtCheck: boolean = atCheck.test(email);
-  const passValid: boolean = password.length > 5;
-  const passwordsEqual = password === confirmPassword;
+  const emailValid: boolean = validateEmail(email);
+  const passValid: boolean = validatePassword(password);
+  const passwordsEqual: boolean = password === confirmPassword;
   const validName: boolean = (firstName !== '' && lastName !== '');
   const validPhone: boolean = validatePhoneNumber(phone);
-  const allValid: boolean = validEmail && multipleAtCheck && passValid && passwordsEqual && validName && validPhone;
+  const allValid: boolean = emailValid && passValid && passwordsEqual && validName && validPhone;
 
   const validationObj: ValidationResponse = {
     valid: allValid,
     errors: []
   };
 
-  const errorMessages: String[] = ['Email is invalid', 'Email is invalid', 'Password is invalid', 'Passwords do not match', 'Please enter a valid name', 'Phone Number is invalid. Please enter without parenthesis or dashes'];
+  const errorMessages: String[] = ['Email is invalid', 'Password is invalid', 'Passwords do not match', 'Please enter a valid name', 'Phone Number is invalid. Please enter without parenthesis or dashes'];
 
-  const validations: boolean[] = [validEmail, multipleAtCheck, passValid, passwordsEqual, validName, validPhone];
+  const validations: boolean[] = [emailValid, passValid, passwordsEqual, validName, validPhone];
 
   if (!allValid) {
     for (let i = 0; i < validations.length; i++) {
       if (!validations[i]) {
-        if (validationObj.errors.indexOf(errorMessages[i]) === -1) {
-          validationObj.errors.push(errorMessages[i]);
-        }
+        validationObj.errors.push(errorMessages[i]);
       }
     }
   }
 
   return validationObj;
-}
-
-function validatePhoneNumber(phone: string) {
-  const phoneLengthValid = phone.length >= 10;
-  return phoneLengthValid;
 }
